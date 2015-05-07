31379bc75556a9c3bf7cc5ccb7666fae95b49eb6
iTAP <iTAP@iTAP-PC10>
2015-05-07 17:03:46 +0900

    write add function and dex function.

--- src/try_Github/Main.java
+++ src/try_Github/Main.java
@@ -5,6 +5,9 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello Git with Eclipse");
 
+		Dev-add add = new Dev-add();
+		Dev-dex dex = new Dex-dex();
+
 	}
 
 }
